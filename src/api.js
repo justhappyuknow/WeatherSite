fb589561fabc03c2f078ab026195159b86e32a26
PARIYASHI SAHU <manpari1979@gmail.com>
2023-06-19 21:27:09 +0530

    Initial Commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd81c4b119emsh58a713f8a7f87d5p1845c3jsna60c46714e84',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY= "9f31d827eee3442090994c4acc6d6779";
